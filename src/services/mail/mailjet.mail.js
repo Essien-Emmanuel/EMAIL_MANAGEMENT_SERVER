d6aa48a885c27489386e2bb40b727684a3c9b590
essien emmanuel <essienemma300@gmail.com>
2024-05-17 09:55:43 +0100

    rm some files and folders

--- src/services/mail/mailjet.mail.js
+++
@@ -1,8 +0,0 @@
-const { GenericMailService } = require('../generics/mail');
-const { MailjetConfiguration } = require('../../database/repositories/mailjetConfiguration.repo');
-
-const configVariables = ['apiKey', 'apiSecret'];
-
-const MailjetMailService = new GenericMailService(configVariables, MailjetConfiguration, 'Mailjet');
-
-exports.MailjetMailService = MailjetMailService;
