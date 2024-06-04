05c225740e861c4f255acccf936e3b61a83bbfbd
essien emmanuel <essienemma300@gmail.com>
2024-06-04 16:22:04 +0100

    added schema for update and id of tag

--- src/validation/schemas/emailTag.js
+++ src/validation/schemas/emailTag.js
@@ -2,12 +2,32 @@ exports.createTagSchema = (req, _res, next) => {
   const schema = {
     tagName: { type: 'string'},
     slug: {type: 'string'},
-    emailRecipients: { type: "arrary", items: { 
-      type: 'object', props: { emaail: { type: "email" } }
+    emailRecipients: { type: "array", items: { 
+      type: 'object', props: { email: { type: "email" } }
     }, optional: true}
   }
   req.schema = {...schema};
   req.input = req.body;
   next();
 
+}
+
+exports.updateTagSchema = (req, _res, next) => {
+  const schema = {
+    tagName: { type: 'string', optional: true},
+    slug: {type: 'string', optional: true},
+    emailRecipients: { type: "array", items: { 
+      type: 'object', props: { email: { type: "email" } }
+    }, optional: true}
+  }
+  req.schema = {...schema};
+  req.input = req.body;
+  next();
+
+}
+
+exports.tagIdSchema = (req, _res, next) => {
+  req.schema = { tagId: idSchemaValue };
+  req.input = { tagId: req.query.tagId};
+  next();
 }
